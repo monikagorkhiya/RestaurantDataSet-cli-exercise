701e20ecae2fcc178772c2e1563e114908183bec
nirav <nirav.gaglani@improwised.com>
2022-11-04 11:37:54 +0530

    Task-6 priceRangeSorting completed

---
+++ priceRangeSorting.js
@@ -0,0 +1,53 @@
+const fileTasks = require("./modules/readCSV.js")
+const filePriceSorting = require("./modules/priceSorting.js")
+
+
+let arr = process.argv[2].split("-");
+const lowerLimit = parseFloat(arr[0]);
+const upperLimit = parseFloat(arr[1]);
+
+const sortingOrder = process.argv[3];
+
+console.log(lowerLimit)
+console.log(upperLimit)
+
+fileTasks.read_csv("assets/restaurant.csv")
+.then(dataSet=>{
+    return priceRangeFilter(dataSet, lowerLimit, upperLimit);
+})
+.then(filteredData=>{
+    return filteredData.sort(filePriceSorting.sortByPriceRange);
+    // Can also write like this her in the next line--> return filteredData
+})
+.then(filteredData=>{
+    printByOrder(filteredData, sortingOrder);
+})
+
+//filtering retaurants based on the given price range
+function priceRangeFilter(dataSet, lowerLimit, upperLimit){
+    let filteredData=[];
+    dataSet.forEach(restro => {
+        let avgCostRestro = parseFloat(restro['Average Cost for two'])
+        if (avgCostRestro>=lowerLimit && avgCostRestro<upperLimit){
+            filteredData.push(restro);
+        }
+    });
+    return filteredData
+}
+
+//function to print restaurants low to high OR high to low as per their pricing
+function printByOrder(filteredData, sortingOrder){
+    let count=0;
+    if (sortingOrder==='low-high'){
+        for(i=filteredData.length-1; i>=0; i--){
+            console.log(filteredData[i]['Restaurant Name'])
+            count++;
+        }
+    }else if(sortingOrder==='high-low'){
+        for(i=0; i<filteredData.length; i++){
+            console.log(filteredData[i]['Restaurant Name'])
+            count++
+        }
+    }
+    console.log("count: ", count)
+}
