7f2adb3ddad6abb240865498b789c7e1dbe8c8ae
nirav <nirav.gaglani@improwised.com>
2022-11-04 17:11:51 +0530

    Added semicolons and gave appropriate variable names

--- ratingWiseRestaurants.js
+++ ratingWiseRestaurants.js
@@ -1,27 +1,27 @@
 const fileReadCSV = require('./modules/readCSV.js');
 const fileSegregateRatings = require('./modules/segregateRatings.js');
-const util = require('util')
+const util = require('util');
 
 fileReadCSV.read_csv("assets/restaurant.csv")
 .then(data=>{
-    return fileSegregateRatings.segregateRatings(data)
+    return fileSegregateRatings.segregateRatings(data);
 })
 .then((segregateObject)=>{
-    printByFashion(segregateObject)
+    printByFashion(segregateObject);
 })
 
 function printByFashion(segregateObject){
-    let var0 = util.format("Rating 0 | White | Not Rated | %s restaurant", segregateObject.notRated);
-    let var1 = util.format("Rating 1.8 to 2.4 | Red | Poor | %s restaurant", segregateObject.poor);
-    let var2 = util.format("Rating 2.5 to 3.4 | Orange | Average | %s restaurant", segregateObject.average);
-    let var3 = util.format("Rating 3.5 to 3.9 | Yellow | Good | %s restaurant", segregateObject.good);
-    let var4 = util.format("Rating 4.0 to 4.4 | Green | Very Good | %s restaurant", segregateObject.veryGood);
-    let var5 = util.format("Rating 4.5 to 4.9 | Dark Green | Excellent | %s restaurant", segregateObject.excellent);
+    let stringNotRated = util.format("Rating 0 | White | Not Rated | %s restaurant", segregateObject.notRated);
+    let stringPoor = util.format("Rating 1.8 to 2.4 | Red | Poor | %s restaurant", segregateObject.poor);
+    let stringAverage = util.format("Rating 2.5 to 3.4 | Orange | Average | %s restaurant", segregateObject.average);
+    let stringGood = util.format("Rating 3.5 to 3.9 | Yellow | Good | %s restaurant", segregateObject.good);
+    let stringVeryGood = util.format("Rating 4.0 to 4.4 | Green | Very Good | %s restaurant", segregateObject.veryGood);
+    let stringExcellent = util.format("Rating 4.5 to 4.9 | Dark Green | Excellent | %s restaurant", segregateObject.excellent);
 
-    console.log(var0)
-    console.log(var1)
-    console.log(var2)
-    console.log(var3)
-    console.log(var4)
-    console.log(var5)
+    console.log(stringNotRated);
+    console.log(stringPoor);
+    console.log(stringAverage);
+    console.log(stringGood);
+    console.log(stringVeryGood);
+    console.log(stringExcellent);
 }
